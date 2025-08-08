780b8ce5ff062848f944d7f595f53c0cf782afce
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-08-08 18:52:36 +0530

    completed mongodb setup

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
-// config
+// configure env
 dotenv.config();
+
+//database config
+connectDB();
 
 // rest object
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
 // rest api
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to E-Commerce App<h1>');
+  res.send('<h1>Welcome to E-Commerce Aplicationp<h1>');
 });
 
 //port
