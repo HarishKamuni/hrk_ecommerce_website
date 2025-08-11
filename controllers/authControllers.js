6b0e5142b6f167b06c0ae621aab2a84bbdf7278d
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-08-11 11:42:13 +0530

    token created

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,6 @@
-import { hashPassword } from '../helpers/authHelper.js';
+import { comparedPassword, hashPassword } from '../helpers/authHelper.js';
 import userModel from '../models/userModel.js';
+import JWT from 'jsonwebtoken';
 
 export const registerController = async (req, res) => {
   try {
@@ -54,6 +55,57 @@ export const registerController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: 'Error in registation',
+      error,
+    });
+  }
+};
+
+//POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: 'Invalid email or password',
+      });
+    }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: 'Email is not registered',
+      });
+    }
+    //compare password
+    const match = await comparedPassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: 'Invalid Password',
+      });
+    }
+    //create token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+    res.status(200).send({
+      success: true,
+      message: 'login successfully',
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token, 
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error in login',
       error,
     });
   }
