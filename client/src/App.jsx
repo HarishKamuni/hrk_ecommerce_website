b51de4b6930ab4e49d2c0bc2f520ab0212b90cb1
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-08-13 19:59:50 +0530

    react setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>Eccomerce App</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-black text-white p-3">
+      <h4 className="text-center">All Right reserved &copy; HRK</h4>
+    </div>
+  );
+};
+
+export default Footer;
