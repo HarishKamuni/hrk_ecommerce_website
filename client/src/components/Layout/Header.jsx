01a402d4f719d17b30a9bc960940191d5d111a22
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-08-15 19:10:28 +0530

    added navbar

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div className="flex items-center justify-between">
+            <Link to={'/'} class="navbar-brand" href="#">
+              Hidden brand
+            </Link>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link
+                  to={'/'}
+                  class="nav-link active"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to={'/'} class="nav-link" href="#">
+                  Link
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
